59f99be2b70a61eb8f75d8ab19db63a6949c40ae
Savin-Infinity <savinpython2010@gmail.com>
2026-05-04 18:47:28 +0530

    Update db by calling the function

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ def init_db():
     conn = sqlite3.connect("users.db")
     c = conn.cursor()
     c.execute("""
-        CREATE TABLE  IF NOT EXISTS auth (
+        CREATE TABLE  IF NOT EXISTS users (
         "id"	INTEGER,
         "username"	TEXT,
         "password"	TEXT,
@@ -57,4 +57,5 @@ def dashboard():
 
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
